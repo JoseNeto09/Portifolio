0cc3d35850e19e2dfc58c3acd8c1b6e3468fb20c
JoseNeto09 <joselopessobrinhoneto9@gmail.com>
2022-10-25 08:44:30 -0300

    logo

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -26,7 +26,7 @@ export const NavBar = () => {
   return (
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
-          <Navbar.Brand href="/">
+          <Navbar.Brand href="/" id="home">
             <img src={logo} alt="Logo" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
