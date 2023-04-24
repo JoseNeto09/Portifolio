f110330dc7a782b21e20b8bf28f9005f665f5add
JoseNeto09 <joselopessobrinhoneto9@gmail.com>
2023-04-24 15:51:37 -0300

    Updates

---
+++ precache-manifest.4e402376e9b42f96114be3dc88b20340.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c2b0df6fc2945af9e5b",
+    "url": "/Portifolio/static/css/main.01af3b77.chunk.css"
+  },
+  {
+    "revision": "7c2b0df6fc2945af9e5b",
+    "url": "/Portifolio/static/js/main.7c2b0df6.chunk.js"
+  },
+  {
+    "revision": "f543a40bef5e0ee902d6",
+    "url": "/Portifolio/static/js/1.f543a40b.chunk.js"
+  },
+  {
+    "revision": "87bfe9323148a13b2edf",
+    "url": "/Portifolio/static/css/2.b8b1c6cf.chunk.css"
+  },
+  {
+    "revision": "87bfe9323148a13b2edf",
+    "url": "/Portifolio/static/js/2.87bfe932.chunk.js"
+  },
+  {
+    "revision": "4cdf63464a2a11a8697b",
+    "url": "/Portifolio/static/js/runtime~main.4cdf6346.js"
+  },
+  {
+    "revision": "e2b8674c67ac891c2d939a3542777f4d",
+    "url": "/Portifolio/static/media/logo.e2b8674c.png"
+  },
+  {
+    "revision": "dea05c56b45857a24dcba5a23202ec70",
+    "url": "/Portifolio/static/media/meter1.dea05c56.svg"
+  },
+  {
+    "revision": "7fb1b05f49ba5c3b4570e1dec3728213",
+    "url": "/Portifolio/static/media/meter2.7fb1b05f.svg"
+  },
+  {
+    "revision": "19e4204daee7b06ccd0aa7807bc37653",
+    "url": "/Portifolio/static/media/meter3.19e4204d.svg"
+  },
+  {
+    "revision": "535f03d00ce745f482617d84bbb8f9ae",
+    "url": "/Portifolio/static/media/header-img.535f03d0.jpg"
+  },
+  {
+    "revision": "d64094774a77a02c2c22356681131cb8",
+    "url": "/Portifolio/static/media/color-sharp.d6409477.png"
+  },
+  {
+    "revision": "d5dfc839d8f030efe0d275cbb8cb9ff0",
+    "url": "/Portifolio/static/media/project-img1.d5dfc839.png"
+  },
+  {
+    "revision": "4129c8f47ee6086db26a33b581941550",
+    "url": "/Portifolio/static/media/project-img2.4129c8f4.png"
+  },
+  {
+    "revision": "a8822e204211be92b2412ccbe284e785",
+    "url": "/Portifolio/static/media/contact-img.a8822e20.svg"
+  },
+  {
+    "revision": "dff2b6246274897c980cd8a354f7f979",
+    "url": "/Portifolio/static/media/nav-icon1.dff2b624.svg"
+  },
+  {
+    "revision": "0393a6c85ddc105c9304f0f7787ddaea",
+    "url": "/Portifolio/static/media/nav-icon2.0393a6c8.svg"
+  },
+  {
+    "revision": "81ff4e970a39053a0acdc10fd58dc5c4",
+    "url": "/Portifolio/static/media/nav-icon3.81ff4e97.svg"
+  },
+  {
+    "revision": "63911e8535278ced52b8a37e819f6f5d",
+    "url": "/Portifolio/static/media/CentraNo2-Medium.63911e85.ttf"
+  },
+  {
+    "revision": "4ea3794d631bedab7999678f46b0a162",
+    "url": "/Portifolio/static/media/CentraNo2-Bold.4ea3794d.ttf"
+  },
+  {
+    "revision": "386f05946a76935fcabf319affff517a",
+    "url": "/Portifolio/static/media/CentraNo2-Book.386f0594.ttf"
+  },
+  {
+    "revision": "e9228c17c13a87653f1b8ed01a3ff306",
+    "url": "/Portifolio/static/media/footer-bg.e9228c17.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/Portifolio/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/Portifolio/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/Portifolio/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "b315f987a0bbabed5319c33bd985d546",
+    "url": "/Portifolio/index.html"
+  }
+];
